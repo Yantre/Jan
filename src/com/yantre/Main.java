33370b7a3d82c6a3eff6110d5313a6bfd3dd92d9
Vasil Hrytskevich <vhrytskevich@eisgroup.com>
2019-03-09 15:36:55 +0300

    Добавили список задач

--- src/com/yantre/Main.java
+++ src/com/yantre/Main.java
@@ -1,8 +1,30 @@
 package com.yantre;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Zadacha> arrayList1 = new ArrayList();
+
+        arrayList1.add(new Zadacha("Zaplatit za kvartiru", 3, 19, 15));
+        arrayList1.add(new Zadacha ("Zaplatit za elektr", 3,19));
+        arrayList1.add(new Zadacha ("Zaplatit za pitanie", 3,19, 20));
+        arrayList1.add(new Zadacha ("Zaplatit za kruzok", 3,19, 22));
+
+
+        arrayList1.sort(Comparator.comparing(Zadacha::getName));
+
+        for (int i = 0; i < arrayList1.size(); i++)
+        {
+            System.out.println(arrayList1.get(i).toString());
+        }
+
+/*        for (Zadacha zadacha : arrayList1)
+        {
+            System.out.println(zadacha.toString());
+        }*/
+
     }
 }
---
+++ src/com/yantre/Zadacha.java
@@ -0,0 +1,95 @@
+package com.yantre;
+
+// объявляем класс
+public class Zadacha
+{
+	public Zadacha () {}
+
+	public Zadacha (String name) {
+		this.name = name;
+	}
+
+
+	// создаем конструктор
+	public Zadacha (String name, int month, int year) {
+
+		this.name = name;
+		this.month = month;
+		this.year = year;
+		this.sostojnie = false;
+		this.endDay = 31;
+	}
+
+	public Zadacha (String name, int month, int year, int endDay) {
+
+		this.name = name;
+		this.month = month;
+		this.year = year;
+		this.sostojnie = false;
+		this.endDay = endDay;
+	}
+
+
+	// аттрибуты класса
+	private String name;
+	private boolean sostojnie;
+	private int month;
+	private int year;
+	private int endDay;
+
+
+	// методы класса
+	public boolean isSostojnie()
+	{
+		return sostojnie;
+	}
+
+	public void setSostojnie(boolean sostojnie)
+	{
+		this.sostojnie = sostojnie;
+	}
+
+	public int getMonth()
+	{
+		return month;
+	}
+
+	public void setMonth(int month)
+	{
+		this.month = month;
+	}
+
+	public int getYear()
+	{
+		return year;
+	}
+
+	public void setYear(int year)
+	{
+		this.year = year;
+	}
+
+	public int getEndDay()
+	{
+		return endDay;
+	}
+
+	public void setEndDay(int endDay)
+	{
+		this.endDay = endDay;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Name:" + this.name + ", Month:"+ this.month +", year:"+this.year + ", sostojnie:" + this.sostojnie + ", endDay: " +this.endDay ;
+	}
+}
